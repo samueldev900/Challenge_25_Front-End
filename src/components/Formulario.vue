eca9429cc7146186d6e8255ab38138fff0d2e859
Samuel Oliveira <samueloliveira900@gmail.com>
2024-07-20 18:57:57 -0300

    atualizando repositório

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,27 +1,33 @@
 <script>
-
+    export default {
+    data() {
+        return {
+        selectedOption: '' // Inicialmente nenhum rádio está selecionado
+        };
+    }
+    };
 </script>
 
 <template>
     <div class="m-6">
         <h1 class="text-3xl font-extrabold text-cor5">Mortage Calculator</h1>
 
-        <p class="border-cor5 w-16 text-sm text-cor5 border-b my-5">Clear All</p>
+        <p class="border-cor5 w-16 text-sm text-cor5 border-b my-2 mb-6">Clear All</p>
         <form action="#">
             <div class="my-4">
                 <label class="" for="amount">Mortage Amount</label>
                 <div class="rounded-lg border border-gray-300 w-80 h-12  flex bg-white justify-between overflow-hidden">
-                    <div class="bg-cor2 w-10 flex items-center justify-center font-bold p-2">
-                        €
+                    <div class="bg-cor1 w-10 flex items-center justify-center font-bold p-2">
+                        £
                     </div>
                     <input type="text" name="amount" class="outline-none w-72 p-4">
                 </div>
             </div>
-            <div class="my-4">
-                <label class="" for="amount">Mortage Term</label>
+            <div class="my-4 ">
+                <label class="p-2" for="amount">Mortage Term</label>
                 <div class="rounded-lg border border-gray-300 w-80 h-12  flex bg-white justify-between overflow-hidden">
                     <input type="text" name="amount" class="outline-none w-72 p-4">
-                    <div class="bg-cor2 w-16 flex items-center justify-center font-bold p-4">
+                    <div class="bg-cor1 w-16 flex items-center justify-center font-bold p-4">
                         Years
                     </div>
                 </div>
@@ -30,25 +36,34 @@
                 <label class="" for="amount">Interest Rate</label>
                 <div class="rounded-lg border border-gray-300 w-80 h-12  flex bg-white justify-between overflow-hidden">
                     <input type="text" name="amount" class="outline-none w-72 p-4">
-                    <div class="bg-cor2 w-16 flex items-center justify-center font-bold p-4">
+                    <div class="bg-cor1 w-16 flex items-center justify-center font-bold p-4">
                         %
                     </div>
                 </div>
             </div>
             <div>
-                <h3 class="my-2">Mortage Type</h3>
-                <div class="rounded-lg border border-gray-300 w-80 h-12  flex items-center bg-white  overflow-hidden p-4 my-2">
-                    <input type="radio" name="rapay" id="repay1">
-                    <label class="px-4" for="repay1">Rapayment</label>
+                <h3 class="my-2">Mortgage Type</h3>
+                <div :class="['rounded-lg border w-80 h-12 flex items-center bg-white overflow-hidden p-4 my-2', selectedOption === 'repay1' ? 'border-cor6 bg-cor10' : 'border-gray-300']">
+                <input type="radio" name="rapay" id="repay1" value="repay1" v-model="selectedOption">
+                <label class="px-4" for="repay1">Repayment</label>
                 </div>
-                <div class="rounded-lg border border-gray-300 w-80 h-12  flex items-center bg-white  overflow-hidden p-4 my-2">
-                    <input type="radio" name="rapay" id="repay2">
-                    <label class="px-4" for="repay2" >Interest Only</label>
+                <div :class="['rounded-lg border w-80 h-12 flex items-center bg-white overflow-hidden p-4 my-2', selectedOption === 'repay2' ? 'border-cor6 bg-cor10' : 'border-gray-300']">
+                <input type="radio" name="rapay" id="repay2" value="repay2" v-model="selectedOption">
+                <label class="px-4" for="repay2">Interest Only</label>
                 </div>
             </div>
             <a class="" href="#">
-                <button class=" w-80 h-12 bg-cor6 rounded-3xl font-bold my-3">
-                    Calculate Repayments
+                <button class="
+                flex
+                items-center
+                justify-center
+                w-80 h-12 
+                bg-cor6 
+                rounded-3xl 
+                font-bold 
+                my-3
+                mt-6">
+                    <img src="../assets/images/icon-calculator.svg" alt=""> Calculate Repayments
                 </button>
             </a>
         </form>
--- src/components/Result.vue
+++ src/components/Result.vue
@@ -3,23 +3,42 @@
 </script>
 
 <template>
-    <div id="resultado">
+    <div class="bg-cor5 p-5">
+        <h1 class="text-cor1 text-2xl font-extrabold my-5">Your Results</h1>
+        <p class="text-cor3 font-semibold">Your results are shown below based on the information you provided. To adjust the results, edit the form and click "calculate repayments" again</p>
+
         
+        <div class="bg-cor9 border-t-4 mt-7 p-2 border-cor6 rounded-md py-5 px-4">
+          <h1 class="text-cor1">Your monthly repayments</h1 >
+          <p class="text-4xl font-bold border-b border-cor4 py-4 text-cor6">£1,797.74</p>
+
+
+          <div class="my-5">
+            <p class="text-cor3">Total you'll repay over the term</p>
+            <p class="text-2xl font-bold  text-cor1 my-2">£539,322.94</p>
+          </div>
+        </div>
+
     </div>
+
 </template>
 
 <style>
+
 :root {
-  --cor1:hsl(202, 86%, 94%);
-  --cor2:hsl(203, 41%, 72%);
+  --cor1: hsl(202, 86%, 94%);
+  --cor2: hsl(203, 41%, 72%);
   --cor3: hsl(200, 26%, 54%);
-  --cor4:hsl(200, 24%, 40%);
-  --cor5:hsl(202, 55%, 16%);
+  --cor4: hsl(200, 24%, 40%);
+  --cor5: hsl(202, 55%, 16%);
+  --lime: hsl(61, 70%, 52%);
+  --red: hsl(4, 69%, 50%);
+  --custom-green: #133040;
+  --custom-green: #0e2431;
 }
 
 #resultado{
     width: 100%;
-    height: 100px;
     background-color: var(--cor5);
 
 }
